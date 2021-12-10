2c9b30e2b8bef964734011afc11458f89583f1a5
Stanley SERBIN <sserbin@e3r1p13.clusters.42paris.fr>
2021-12-10 18:25:31 +0100

    passing structure properly

--- tests/test_mutex.c
+++ tests/test_mutex.c
@@ -6,16 +6,17 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 17:55:36 by sserbin           #+#    #+#             */
-/*   Updated: 2021/12/10 18:07:04 by sserbin          ###   ########.fr       */
+/*   Updated: 2021/12/10 18:25:04 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 typedef struct s_root {
-	pthread_mutex_t	g_mutex;
+	pthread_mutex_t	*g_mutex;
 	char			*name;
 }	t_root;
 
@@ -24,9 +25,9 @@ void	*routine(void *arg)
 	t_root	*root;
 
 	root = (t_root *)arg;
-	pthread_mutex_lock(&root->g_mutex);
+	pthread_mutex_lock(root->g_mutex);
 	printf("%s\n", root->name);
-	pthread_mutex_unlock(&root->g_mutex);
+	pthread_mutex_unlock(root->g_mutex);
 	return (arg);
 }
 
@@ -35,14 +36,18 @@ int	main(void)
 	pthread_t		threads[4];
 	char			*names[] = {"Aristote", "Socrate", "Platon", "Kant", 0};
 	unsigned int	i;
-	t_root			root;
+	t_root			*root;
+	pthread_mutex_t	g_mutex;
 
-	pthread_mutex_init(&root.g_mutex, NULL);
+	pthread_mutex_init(&g_mutex, NULL);
 	i = 0;
+	root = NULL;
 	while (names[i])
 	{
-		root.name = names[i];
-		pthread_create(&threads[i], NULL, routine, &root);
+		root = malloc(sizeof(t_root));
+		root->name = names[i];
+		root->g_mutex = &g_mutex;
+		pthread_create(&threads[i], NULL, routine, root);
 		i++;
 	}
 	i = 0;
@@ -51,6 +56,6 @@ int	main(void)
 		pthread_join(threads[i], NULL);
 		i++;
 	}
-	pthread_mutex_destroy(&root.g_mutex);
+	pthread_mutex_destroy(&g_mutex);
 	return (0);
 }
