f0f701f7da1447c209c2d0313937d71692266557
Stanley SERBIN <stanleyserbin@gmail.com>
2021-12-12 12:02:58 +0100

    protect fork with mutex ok

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 18:57:04 by sserbin           #+#    #+#             */
-/*   Updated: 2021/12/12 11:30:40 by sserbin          ###   ########.fr       */
+/*   Updated: 2021/12/12 12:02:00 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ typedef struct s_couvert {
 	t_dishes	*right;
 }	t_couvert;
 
-t_couvert	get_philo_fork(unsigned int id, t_dishes *fork, char *name)
+t_couvert	get_philo_fork(unsigned int id, t_dishes *fork)
 {
 	t_couvert	to_return;
 	t_dishes	*tmp;
@@ -80,39 +80,45 @@ t_couvert	get_philo_fork(unsigned int id, t_dishes *fork, char *name)
 	}
 	else
 		to_return.right = tmp;
-	printf("Philo %s with id %d ; left fork %d ; right fork %d\n", name, copy_id, to_return.left->id, to_return.right->id);
 	return (to_return);
 }
 
 void	*routine(void *arg)
 {
 	t_root			*root;
-	// struct timeval	time;
-	// long int		time_to_die;
+	struct timeval	time;
+	long int		time_to_die;
+	t_couvert		couvert;
 
 	root = (t_root *)arg;
-	get_philo_fork(root->id, root->fork, root->name);
-	// printf("creating philo %s with id %d\n", root->name, root->id);
-	// gettimeofday(&time, NULL);
-	// time_to_die = time.tv_sec + 5;
-	// while (1)
-	// {
-	// 	gettimeofday(&time, NULL);
-	// 	if (time.tv_sec >= time_to_die)
-	// 		break ;
-	// 	gettimeofday(&time, NULL);
-	// 	philo_is_eating(time, 2, root);
-	// 	gettimeofday(&time, NULL);
-	// 	if (time.tv_sec >= time_to_die)
-	// 		break ;
-	// 	gettimeofday(&time, NULL);
-	// 	philo_is_sleeping(time, 2, root->name);
-	// 	gettimeofday(&time, NULL);
-	// 	if (time.tv_sec >= time_to_die)
-	// 		break ;
-	// 	gettimeofday(&time, NULL);
-	// 	philo_is_thinking(time, 2, root->name);
-	// }
-	// printf("%s just die\n", root->name);
+	couvert = get_philo_fork(root->id, root->fork);
+	printf("creating philo %s with id %d\n", root->name, root->id);
+	gettimeofday(&time, NULL);
+	time_to_die = time.tv_sec + 5;
+	while (1)
+	{
+		gettimeofday(&time, NULL);
+		if (time.tv_sec >= time_to_die)
+			break ;
+		gettimeofday(&time, NULL);
+		pthread_mutex_lock(&couvert.right->fork);
+		pthread_mutex_lock(&couvert.left->fork);
+		gettimeofday(&time, NULL);
+		printf("%ld %s has taken a fork\n", time.tv_sec, root->name);
+		philo_is_eating(time, 2, root);
+		pthread_mutex_unlock(&couvert.right->fork);
+		pthread_mutex_unlock(&couvert.left->fork);
+		gettimeofday(&time, NULL);
+		if (time.tv_sec >= time_to_die)
+			break ;
+		gettimeofday(&time, NULL);
+		philo_is_sleeping(time, 2, root->name);
+		gettimeofday(&time, NULL);
+		if (time.tv_sec >= time_to_die)
+			break ;
+		gettimeofday(&time, NULL);
+		philo_is_thinking(time, 2, root->name);
+	}
+	printf("%s just die\n", root->name);
 	return (free_root_and_return(root));
 }
