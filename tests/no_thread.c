58db5050b487acd86288903b12aea6fef1addf61
Stanley SERBIN <sserbin@e2r5p11.clusters.42paris.fr>
2021-12-16 23:02:14 +0100

    aaaaaah

--- tests/no_thread.c
+++ tests/no_thread.c
@@ -6,7 +6,7 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 20:29:50 by sserbin           #+#    #+#             */
-/*   Updated: 2021/12/16 21:52:21 by sserbin          ###   ########.fr       */
+/*   Updated: 2021/12/16 23:01:52 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <pthread.h>
 
 #define ONE_SECOND	1000000
+#define ONE_MINI_SECOND 1000
 #define EATING		0
 #define SLEEPING	1
 #define THINKING	2
@@ -27,12 +28,12 @@ void	action(int action, unsigned int id)
 	if (action == EATING)
 	{
 		printf("philo %d is eating\n", id);
-		usleep(ONE_SECOND * 2);
+		usleep(ONE_MINI_SECOND * 2);
 	}
 	else if (action == SLEEPING)
 	{
 		printf("philo %d is sleeping\n", id);
-		usleep(ONE_SECOND * 2);
+		usleep(ONE_MINI_SECOND * 2);
 	}
 	else
 	{
@@ -45,16 +46,25 @@ void	*routine(void *arg)
 	unsigned int	iter;
 	unsigned int	*thread_nb;
 	struct timeval	time;
+	long int		total;
+	long int		tmp;
 
 	pthread_mutex_lock(&g_mutex);
 	iter = 0;
 	thread_nb = (unsigned int *)arg;
 	while (iter < 4)
 	{
+		total = 0;
+		gettimeofday(&time, NULL);
+		total = total + time.tv_usec;
+		tmp = total;
 		action(EATING, *thread_nb);
 		action(SLEEPING, *thread_nb);
 		action(THINKING, *thread_nb);
 		gettimeofday(&time, NULL);
+		total = total + time.tv_usec;
+		printf("** Philo %d took %lu %lu (minisecond) to made iteration %d**\n",
+			*thread_nb, (total % tmp) / ONE_MINI_SECOND, (total % tmp), iter);
 		iter++;
 	}
 	pthread_mutex_unlock(&g_mutex);
