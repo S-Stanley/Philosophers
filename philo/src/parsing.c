a1ffa4548860245ce270e718c377ce3c607f62ef
Stanley SERBIN <sserbin@e2r1p9.clusters.42paris.fr>
2022-01-11 20:45:11 +0100

    parsing in progress

--- philo/src/parsing.c
+++ philo/src/parsing.c
@@ -6,15 +6,41 @@
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 23:03:52 by sserbin           #+#    #+#             */
-/*   Updated: 2022/01/03 23:13:27 by sserbin          ###   ########.fr       */
+/*   Updated: 2022/01/11 20:44:20 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
+int	ft_strlen(const char *str)
+{
+	int		i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
 BOOL	print_and_return(void)
 {
 	printf("Wrong number or arguments\n");
+	return (TRUE);
+}
+
+BOOL	is_arg_correct(char **argv, t_arg arg)
+{
+	int		i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (ft_strlen(argv[i]) == 0)
+			return (FALSE);
+		i++;
+	}
 	return (TRUE);
 }
 
@@ -59,5 +85,6 @@ t_arg	setup_arg(int argc, char **argv)
 		arg.max_t_eat = ft_atoi(argv[5]);
 	else
 		arg.max_t_eat = -1;
+	printf("%d %d %d %d %d\n", arg.nbr_philo, arg.t_die, arg.t_eat, arg.t_sleep, arg.max_t_eat);
 	return (arg);
 }
