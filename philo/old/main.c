193ba7d3f4d1dab88956402bba9d4cc9669de386
Stanley SERBIN <sserbin@e2r8p6.clusters.42paris.fr>
2022-01-12 19:15:23 +0100

    tests are working

---
+++ philo/old/main.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/02 17:52:15 by sserbin           #+#    #+#             */
+/*   Updated: 2022/01/11 21:31:47 by sserbin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+BOOL	check_philo_life(struct timeval start_time, t_data *data)
+{
+	if (data->stop[0])
+	{
+		pthread_mutex_unlock(data->mutex);
+		return (FALSE);
+	}
+	if (get_time(start_time) > (unsigned int)data->t_die)
+	{
+		data->stop[0] = 1;
+		printf("%ld\n", get_time(start_time));
+		printf("%ld philo %d died\n", get_time(data->time), data->id);
+		pthread_mutex_unlock(data->mutex);
+		return (FALSE);
+	}
+	return (TRUE);
+}
+
+t_data	*create_data(int id, pthread_mutex_t *mutex, t_arg arg, int *stop)
+{
+	t_data	*data;
+
+	data = malloc(sizeof(t_data));
+	if (!data)
+		return (NULL);
+	data->id = id;
+	data->mutex = mutex;
+	data->t_sleep = arg.t_sleep;
+	data->t_eat = arg.t_eat;
+	data->t_die = arg.t_die;
+	data->max_t_eat = arg.max_t_eat;
+	data->stop = stop;
+	return (data);
+}
+
+void	join_philo(t_philo *philo, int *stop)
+{
+	while (philo)
+	{
+		if (pthread_join(philo->thread, NULL) != 0)
+			return ;
+		philo = philo->next;
+	}
+	free(stop);
+}
+
+void	start_program(t_arg arg, t_philo *philo, t_dishes *fork)
+{
+	t_philo			*tmp;
+	struct timeval	time;
+	pthread_mutex_t	mutex;
+	t_data			*data;
+	int				*stop;
+
+	gettimeofday(&time, NULL);
+	tmp = philo;
+	pthread_mutex_init(&mutex, NULL);
+	stop = create_stop_var();
+	while (philo)
+	{
+		data = create_data(philo->id, &mutex, arg, stop);
+		if (!data)
+			return ;
+		data->time = time;
+		data->fork = fork;
+		data->philo = tmp;
+		if (pthread_create(&philo->thread, NULL, routine, data) != 0)
+			return ;
+		philo = philo->next;
+	}
+	join_philo(tmp, stop);
+	pthread_mutex_destroy(&mutex);
+}
+
+int	main(int argc, char **argv)
+{
+	t_arg			arg;
+	t_philo			*philo;
+	t_dishes		*fork;
+
+	if (check_error_arg(argc, argv))
+		return (0);
+	arg = setup_arg(argc, argv);
+	if (!is_arg_correct(argv, arg, argc))
+		return (0);
+	philo = setup_philo(arg);
+	if (!philo)
+		return (0);
+	fork = init_mutex(arg.nbr_philo);
+	if (!fork)
+	{
+		free_for_exit(fork, philo);
+		return (0);
+	}
+	start_program(arg, philo, fork);
+	free_for_exit(fork, philo);
+	return (0);
+}
--- philo/src/main.c
+++ philo/src/main.c
@@ -5,108 +5,157 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/02 17:52:15 by sserbin           #+#    #+#             */
-/*   Updated: 2022/01/11 21:31:47 by sserbin          ###   ########.fr       */
+/*   Created: 2022/01/11 23:48:31 by sserbin           #+#    #+#             */
+/*   Updated: 2022/01/12 19:14:49 by sserbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <unistd.h>
 
-BOOL	check_philo_life(struct timeval start_time, t_data *data)
+#define NB_PHILO		4
+#define TIME_TO_DIE		410
+#define TIME_TO_SLEEP	200
+#define TIME_TO_EAT		200
+
+typedef struct s_data {
+	int				id;
+	pthread_mutex_t	*forks;
+	struct timeval	prog_time_start;
+}	t_data;
+
+long int	get_time(struct timeval time)
 {
-	if (data->stop[0])
-	{
-		pthread_mutex_unlock(data->mutex);
-		return (FALSE);
-	}
-	if (get_time(start_time) > (unsigned int)data->t_die)
-	{
-		data->stop[0] = 1;
-		printf("%ld\n", get_time(start_time));
-		printf("%ld philo %d died\n", get_time(data->time), data->id);
-		pthread_mutex_unlock(data->mutex);
-		return (FALSE);
-	}
-	return (TRUE);
+	struct timeval	now;
+	long int		max;
+
+	max = 999999;
+	gettimeofday(&now, NULL);
+	if (now.tv_sec == time.tv_sec)
+		return ((now.tv_usec - time.tv_usec) / 1000);
+	else
+		return ((
+				((max - time.tv_usec) + now.tv_usec)
+				+ ((now.tv_sec - time.tv_sec -1) * max)) / 1000);
 }
 
-t_data	*create_data(int id, pthread_mutex_t *mutex, t_arg arg, int *stop)
+void	lock_fork(t_data *data)
 {
-	t_data	*data;
-
-	data = malloc(sizeof(t_data));
-	if (!data)
-		return (NULL);
-	data->id = id;
-	data->mutex = mutex;
-	data->t_sleep = arg.t_sleep;
-	data->t_eat = arg.t_eat;
-	data->t_die = arg.t_die;
-	data->max_t_eat = arg.max_t_eat;
-	data->stop = stop;
-	return (data);
-}
-
-void	join_philo(t_philo *philo, int *stop)
-{
-	while (philo)
+	if (data->id == NB_PHILO)
 	{
-		if (pthread_join(philo->thread, NULL) != 0)
-			return ;
-		philo = philo->next;
+		pthread_mutex_lock(&data->forks[data->id - 1]);
+		printf("%ld philo %d has taken a fork\n",
+			get_time(data->prog_time_start), data->id);
+		pthread_mutex_lock(&data->forks[0]);
+		printf("%ld philo %d has taken a fork\n",
+			get_time(data->prog_time_start), data->id);
 	}
-	free(stop);
+	else
+	{
+		pthread_mutex_lock(&data->forks[data->id - 1]);
+		printf("%ld philo %d has taken a fork\n",
+			get_time(data->prog_time_start), data->id);
+		pthread_mutex_lock(&data->forks[data->id]);
+		printf("%ld philo %d has taken a fork\n",
+			get_time(data->prog_time_start), data->id);
+	}	
 }
 
-void	start_program(t_arg arg, t_philo *philo, t_dishes *fork)
+void	eating(t_data *data, struct timeval *start_time)
 {
-	t_philo			*tmp;
-	struct timeval	time;
-	pthread_mutex_t	mutex;
+	lock_fork(data);
+	if (get_time(*start_time) >= TIME_TO_DIE)
+	{
+		printf("%ld philo %u died\n", get_time(*start_time), data->id);
+		exit(0);
+	}
+	printf("%ld philo %d is eating\n", get_time(data->prog_time_start), data->id);
+	printf("**** philo %d max %ld ****\n", data->id, get_time(*start_time));
+	gettimeofday(start_time, NULL);
+	usleep(TIME_TO_EAT * 1000);
+	pthread_mutex_unlock(&data->forks[data->id - 1]);
+	if (data->id == NB_PHILO)
+		pthread_mutex_unlock(&data->forks[0]);
+	else
+		pthread_mutex_unlock(&data->forks[data->id]);
+}
+
+void	*routine(void *arg)
+{
 	t_data			*data;
-	int				*stop;
+	struct timeval	start_time;
 
-	gettimeofday(&time, NULL);
-	tmp = philo;
-	pthread_mutex_init(&mutex, NULL);
-	stop = create_stop_var();
-	while (philo)
+	data = (t_data *)arg;
+	gettimeofday(&start_time, NULL);
+	if (data->id % 2)
+		usleep(200 * 1000);
+	while (1)
 	{
-		data = create_data(philo->id, &mutex, arg, stop);
-		if (!data)
-			return ;
-		data->time = time;
-		data->fork = fork;
-		data->philo = tmp;
-		if (pthread_create(&philo->thread, NULL, routine, data) != 0)
-			return ;
-		philo = philo->next;
+		eating(data, &start_time);
+		printf("%ld philo %d is sleeping\n",
+			get_time(data->prog_time_start), data->id);
+		usleep(TIME_TO_SLEEP * 1000);
+		printf("%ld philo %d is thinking\n",
+			get_time(data->prog_time_start), data->id);
 	}
-	join_philo(tmp, stop);
-	pthread_mutex_destroy(&mutex);
+	free(arg);
+	return (arg);
 }
 
-int	main(int argc, char **argv)
+void	init_mutex(pthread_mutex_t *forks)
 {
-	t_arg			arg;
-	t_philo			*philo;
-	t_dishes		*fork;
+	int	i;
 
-	if (check_error_arg(argc, argv))
-		return (0);
-	arg = setup_arg(argc, argv);
-	if (!is_arg_correct(argv, arg, argc))
-		return (0);
-	philo = setup_philo(arg);
-	if (!philo)
-		return (0);
-	fork = init_mutex(arg.nbr_philo);
-	if (!fork)
+	i = 0;
+	while (i < NB_PHILO)
 	{
-		free_for_exit(fork, philo);
-		return (0);
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
 	}
-	start_program(arg, philo, fork);
-	free_for_exit(fork, philo);
+}
+
+void	destroy_mutex(pthread_mutex_t *forks)
+{
+	int	i;
+
+	i = 0;
+	while (i < NB_PHILO)
+	{
+		pthread_mutex_destroy(&forks[i]);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	pthread_t		threads[NB_PHILO];
+	void			*forks;
+	int				i;
+	t_data			*data;
+	struct timeval	prog_time_start;
+
+	i = 0;
+	forks = malloc(sizeof(pthread_mutex_t) * NB_PHILO);
+	init_mutex(forks);
+	gettimeofday(&prog_time_start, NULL);
+	while (i < NB_PHILO)
+	{
+		data = malloc(sizeof(t_data));
+		data->id = i + 1;
+		data->forks = forks;
+		data->prog_time_start = prog_time_start;
+		pthread_create(&threads[i], NULL, routine, data);
+		i++;
+	}
+	i = 0;
+	while (i < NB_PHILO)
+	{
+		pthread_join(threads[i], NULL);
+		i++;
+	}
+	destroy_mutex(forks);
 	return (0);
 }
